1267837797a3b508b4dc0ff8a527d0a34e994ca2
Lívia Maria <livia_pereira150@estudante.sesisenai.org.br>
2026-09-08 12:19:19 -0300

    segunda versao

--- models/Copo.js
+++ models/Copo.js
@@ -1,10 +1,11 @@
 class Copo {
     constructor(raioMaior, raioMenor, altura) {
-        this.raioMaior = raioMaior;
-        this.raioMenor = raioMenor;
-        this.altura = altura;
+        this.raioMaior = raioMaior; // R
+        this.raioMenor = raioMenor; // r
+        this.altura = altura;       // h
     }
 
+    // g = √(h² + (R - r)²)
     calcularGeratriz() {
         return Math.sqrt(
             this.altura ** 2 +
@@ -12,30 +13,36 @@ class Copo {
         );
     }
 
+    // Ab = π · r²
     calcularAreaBaseMenor() {
         return Math.PI * this.raioMenor ** 2;
     }
 
+    // AB = π · R²
     calcularAreaBaseMaior() {
         return Math.PI * this.raioMaior ** 2;
     }
 
+    // Al = π · g · (R + r)
     calcularAreaLateral() {
-        const geratriz = this.calcularGeratriz();
+        const g = this.calcularGeratriz();
 
-        return Math.PI * geratriz *
+        return Math.PI * g *
             (this.raioMaior + this.raioMenor);
     }
 
+    // V = (π · h / 3) · (R² + R · r + r²)
     calcularVolume() {
-        return (Math.PI * this.altura / 3) *
-            (
-                this.raioMaior ** 2 +
-                this.raioMaior * this.raioMenor +
-                this.raioMenor ** 2
-            );
+        return (
+            Math.PI * this.altura / 3
+        ) * (
+            this.raioMaior ** 2 +
+            this.raioMaior * this.raioMenor +
+            this.raioMenor ** 2
+        );
     }
 
+    // Classificação do copo pelo volume
     classificarVolume() {
         const volume = this.calcularVolume();
 
@@ -48,12 +55,15 @@ class Copo {
         }
     }
 
+    // Área total = AB + Ab + Al
     calcularAreaTotal() {
         const areaBaseMaior = this.calcularAreaBaseMaior();
         const areaBaseMenor = this.calcularAreaBaseMenor();
         const areaLateral = this.calcularAreaLateral();
 
-        return areaBaseMaior + areaBaseMenor + areaLateral;
+        return areaBaseMaior +
+               areaBaseMenor +
+               areaLateral;
     }
 }
 
